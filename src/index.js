9b8fb17f8f7193bee47265a3b24d4371252bfd68
Rob Whitaker <rmw2@princeton.edu>
2018-06-13 22:13:33 -0400

    the last of the semicolons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import { syncHistoryWithStore } from 'react-router-redux'
