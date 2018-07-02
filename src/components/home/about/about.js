93d8c51b8792f791758c85023c1ac302cab95388
Siunami <Siunami.matt@gmail.com>
2018-07-02 16:21:50 -0700

    quick push

--- src/components/home/about/about.js
+++ src/components/home/about/about.js
@@ -6,12 +6,7 @@ class About extends Component {
         <main className="container">
             <div className="row">
                 <div className="column center aligned">
-                    <p>hi</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="column center aligned">
-                    <h1 className="banner-head">Create Proof of Attendence badges for your events with uPort Live</h1>
+                    <h1 className="banner-head">About</h1>
                 </div>
             </div>
         </main>
