c572747a183220ba2fb52efd1cc29e1648bc24a2
Siunami <Siunami.matt@gmail.com>
2018-07-03 09:11:54 -0700

    about markdown file added

--- src/components/home/about/about.js
+++ src/components/home/about/about.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
+import Markdown from 'markdown-to-jsx'
+
+import a from '../../../../about.md'
 
 class About extends Component {
   render() {
     return (
       <main className="container">
-        <div className="row">
-          <div className="column center aligned">
-            <h1 className="banner-head">About</h1>
-          </div>
-        </div>
-      </main>
+      <div className="fullpage" style={{textAlign: 'left'}}>
+        <Markdown>
+          {a}
+        </Markdown>
+      </div>
+    </main>
     )}
 }
 
